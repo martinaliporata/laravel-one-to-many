47099bf37cfceeb1d53e9b930cc9db724668c950
martinaliporata <martinaliporata@gmail.com>
2024-07-30 19:52:04 +0200

    added foreign key to main table

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,8 +15,13 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
+        // così prendo le categorie; oltre a ciò, devo aggiungere gli id delle categorie con il pluck
+        // che restituisci i valori di una determinata chiave (pluck->(id) vuol dire dammi solo gli id)
+        $types= Type::all()->pluck('id');
         for($i=0; $i<50; $i++){
             $newProject = new Project();
+            // dammi un random che viene dalla tabella types
+            $newProject->type_id = $faker->randomElement($types);
             $newProject->title=$faker->realText(40);
             $newProject->author=$faker->name();
             $newProject->date=$faker->dateTimeThisMonth();
